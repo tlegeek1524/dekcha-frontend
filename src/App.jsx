4feeffe86c630eb7e7314791e202813f5566f2d8
tlegeek1524 <tletletle2546@gmail.com>
2025-07-03 00:54:52 +0700

    update app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 // App.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserProfile from './component/pages/UserProfile';
 import Login from './component/auth/login';
 import Adminlogin from './component/auth/adminlogin';
-import Employeeindex from './component/employeepage/employeeindex'
+import Employeeindex from './component/employeepage/employeeindex';
 import Usermenu from './component/pages/usermenu';
 
 function App() {
   return (
-
+    <BrowserRouter>
       <Routes>
-]]
-        <Route path="/" element=  {<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login/userlogin" element={<UserProfile />} />
         <Route path="/auth/employee" element={<Employeeindex />} />
         <Route path="/auth/login" element={<Adminlogin />} />
         <Route path="/login/menu" element={<Usermenu />} />
       </Routes>
-
+    </BrowserRouter>
   );
 }
 
